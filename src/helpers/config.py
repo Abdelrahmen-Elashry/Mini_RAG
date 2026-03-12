86eb567924055b6d019b44ba2d94202af9ef6721
Abdelrahmen Elashry <abdelrahmenelashry@gmail.com>
2026-03-12 13:50:20 +0200

    Defined get_settings function

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -4,6 +4,9 @@ class Settings(BaseSettings):
     APP_NAME: str
     APP_VERSION: str
     OPENAI_API_KEY: str
-    
+
     class config():
         env_file = ".env"
+
+def get_settings():
+    return Settings()
