91ba7c1ee33c780a311c57c50b3a4e97e136d535
Abdelrahmen Elashry <abdelrahmenelashry@gmail.com>
2026-03-13 07:44:47 +0200

    Add the data route

--- src/routes/data.py
+++ src/routes/data.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI, APIRouter, Depends, UploadFile, status
 from fastapi.responses import JSONResponse
 from helpers.config import get_settings, Settings
-from controllers import DataController
+from controllers import DataController, ProjectController
+from models import ResponseSignal
+import aiofiles
 import os
+import logging
+
+logger = logging.getLogger('uvicorn.error')
 
 data_router = APIRouter(
     prefix="/api/v1/data",
@@ -22,10 +27,27 @@ async def upload_data(project_id: str, file: UploadFile,
                 "signal": result_signal
             }
         )
-    
-    return JSONResponse(
-        status_code=status.HTTP_200_OK,
-        content={
-            "signal": result_signal
-        }
+
+    file_path, file_id = DataController().generate_unique_filepath(
+        orig_file_name=file.filename,
+        project_id=project_id
     )
+
+    try:
+        async with aiofiles.open(file_path, "wb") as f:
+                while chunk := await file.read(app_settings.FILE_DEFAULT_CHUNK_SIZE):
+                    await f.write(chunk)
+    except Exception as e:
+         logger.error(f"Error while uploading file: {e}")
+         return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                "signal": ResponseSignal.FILE_UPLOAD_FAILED.value
+            }
+        )
+
+    return JSONResponse(
+            content={
+                "signal": ResponseSignal.FILE_UPLOAD_SUCCESS.value
+            }
+        )
